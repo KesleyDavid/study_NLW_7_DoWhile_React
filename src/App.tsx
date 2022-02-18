3a854937d634096766a6741e3452c0760972cd76
Kesley DEV <kesleygti@gmail.com>
2022-02-18 14:26:20 -0300

    feat: create login box

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import styles from './App.module.css';
+import styles from './App.module.scss';
+
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
 
 export function App() {
   return (
     <main className={styles.contentWrapper}>
-      <h1>Kesley</h1>
+      <MessageList />
+      <LoginBox />
     </main>
   )
 }
---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}></div>
+  )
+}
