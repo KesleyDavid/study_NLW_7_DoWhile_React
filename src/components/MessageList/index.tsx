1b0d09307b5f60fbd7859c9f4e52aabf78260819
Kesley DEV <kesleygti@gmail.com>
2022-02-18 14:40:39 -0300

    feat: create message list

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,47 @@
+import logoImg from '../../assets/logo.svg';
+
 import styles from './styles.module.scss';
 
 export function MessageList() {
   return (
-    <div className={styles.messageListWrapper}></div>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            I can't wait to start this event, it's definitely going to be the best of all time, let's go! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/kesleydavid.png" alt="Kesley" />
+            </div>
+            <span>Kesley</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            This one is going to be just amazing! Let's learn everything about the assembly of GraphQL APIs. Not to mention the lectures and panels.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/kesleydavid.png" alt="Kesley" />
+            </div>
+            <span>Kesley</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Undoubtedly the lectures will be useful for my career and for many 😍 #gorocket
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/kesleydavid.png" alt="Kesley" />
+            </div>
+            <span>Kesley</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
